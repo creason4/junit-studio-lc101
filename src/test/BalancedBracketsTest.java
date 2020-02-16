642c5358341754f091cc477da9a8fe576ba638e5
Laura Creason <creason4@gmail.com>
2020-02-15 22:28:50 -0600

    Unit Testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -12,5 +12,60 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testHasBalancedBracketsNull() {
+        BalancedBrackets.hasBalancedBrackets(null);
+    }
 
+    @Test
+    public void testBad() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testHasBalancedBracketsTrueEmpty() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testHasBalancedBracketsTrue1() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsTrue2() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsTrue3() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsTrue4() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsTrueNested() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Launch[]Code]"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsFalse1() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testHasBalancedBracketsFalse2() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testHasBalancedBracketsFalse3() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
 }
+
+
